b4699fd0d92f4faaf9078882d8e7c497110eb942
Chiu-Te-Wang <qwas65166516@gmail.com>
2014-06-02 17:32:21 +0800

    test fb login by Chiu

--- app.js
+++ app.js
@@ -19,71 +19,15 @@ app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(express.methodOverride());//[add]
-app.use(express.cookieParser(process.env.COOKIE_SECRET));//[add]
-app.use(express.session());//[add]
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(app.router);
-//-------------------fb login test------------------
-// https://github.com/jaredhanson/passport#middleware
-require('./routes/passport'); // require Passport configuration
-require('./models/user');
-var flash = require('connect-flash');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var User = mongoose.model('User'); 
 
-app.use(passport.initialize());
-app.use(passport.session());
-// Session based flash messages
-app.use(flash());
-
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/mylist', function(req, res,next){
-    passport.authenticate('facebook')(req, res, next);
-});
-
-app.get('/fbcb', passport.authenticate('facebook', {
-    successRedirect:'/login',
-    failureRedirect: '/food'
-}));
-
-app.get('/login' ,function(req,res){
-    var fbid = req.user && req.user.id; // Get user from req.user
-    req.logout(); // Delete req.user
-    // Redirect the malicious (not logged in) requests.
-    //
-    if(fbid === undefined ){
-      req.flash('info', "請先登入FaceBook。");
-      return res.redirect('/food');
-    }
-    var user = new User({fbid: fbid});
-    user.save(function(err,newUser){
-        if( err ){
-            req.flash('info', "已是使用者！");
-            return res.redirect('/');
-        }
-        else{
-            req.flash('info',"good!!!");
-        }
-    });
-    //mylist();
-});
-
-
-//------------------------------------
 
 app.get('/', routes.index);
 app.get('/food', routes.food);
-//app.get('/mylist', routes.mylist);
+app.get('/mylist', routes.mylist);
 app.get('/events', routes.events);
 
 /// catch 404 and forwarding to error handler
